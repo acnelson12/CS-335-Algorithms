29f610ab276e8f21e9603b84fe48624b89be43d7
Aaron Nelson <acnelson12@gmail.com>
2015-10-23 10:33:35 -0500

    Update Seam.java

--- Seam-Carving/Seam.java
+++ Seam-Carving/Seam.java
@@ -1,118 +1,203 @@
-//Implementation is secret for now.
+/**
+ * Class to handle image resizing by seam-carving.
+ **/
+
 public class Seam
 {
-	public  void verticalSeamShrink( UWECImage im );
+	/* Instance Constants */
+	private final int WAIT;
 	
-	public  void horizontalSeamShrink( UWECImage im );
+	/*== Constructors ==*/
 	
 	/**
-	 * Fast Vertical Seam Shrink.
-	 * Shrinks the image by removing vertical seams and attempts
-	 * to maximize speed and efficiency rather than showcasing the
-	 * process. (not required by specs)
+	 * Constructs a Seam with 0 for its wait time.
+	 **/
+	public Seam()
+	{
+		this(0);
+	}
+	
+	/**
+	 * Constructs a Seam with a given wait time.
+	 * 
+	 * @param WAIT
+	 * 		time (in ms) to wait after drawing a seam.
+	 **/
+	public Seam( final int WAIT )
+	{
+		this.WAIT = WAIT;
+	}
+	
+	/*== Accessors ==*/
+	
+	/**
+	 * Shrinks a given image by removing a vertical seam.
 	 * 
 	 * @param im
-	 * 	the image to operate on.
-	 * @param decWidth
-	 * 	the amount to decrease the width.
+	 * 		the image.
 	 **/
-	public void fastVSShrink( UWECImage im, int decWidth )
+	public void verticalSeamShrink( UWECImage im );
+	
+	/**
+	 * Shrinks a given image by removing a horizontal seam.
+	 * 
+	 * @param im
+	 * 		the image.
+	 **/
+	public void horizontalSeamShrink( UWECImage im );
+	
+	/**
+	 * Shrinks a given image by removing a given number
+	 * of vertical seams.
+	 * <p>
+	 * Unlike verticalSeamShrink, this method is oriented
+	 * towards resizing the image as quickly as possible,
+	 * rather than showcasing the process.
+	 * 
+	 * @param im
+	 * 		the image.
+	 * @param DEC_WIDTH
+	 * 		the number of vertical seams to remove.
+	 **/
+	public void fastVSShrink( UWECImage im, final int DEC_WIDTH )
 	{
+		/* Local Variables */
 		int[][] energy      = null;
 		int[][] pathWeights = null;
 		int[]   seam        = null;
 		
-		if ( 0 < decWidth )
+		/* Run initial calculations */
+		if ( 0 < DEC_WIDTH )
 		{
 			energy      = getEnergy( im );
-			pathWeights = getPathWeights( im, energy );
-			seam        = getSeam( im, pathWeights );
+			pathWeights = getPathWeights( energy );
+			seam        = getSeam( pathWeights );
+			
+			/* Remove Seam */
 			im.switchImage( im.shrinkImage( seam ) );
 		}
 		
-		for ( int i = 1; i < decWidth; i++ )
+		/* Run optimized calculations */
+		for ( int i = 1; i < DEC_WIDTH; i++ )
 		{
 			energy      = getEnergy( im, energy, seam );
-			pathWeights = getPathWeights( im, energy, pathWeights, seam );
-			seam        = getSeam( im, pathWeights );
+			pathWeights = getPathWeights( energy, pathWeights, seam );
+			seam        = getSeam( pathWeights );
+			
+			/* Remove Seam */
 			im.switchImage( im.shrinkImage( seam ) );
 		}
 		im.repaintCurrentDisplayWindow();
 	}
 	
 	/**
-	 * Fast Horizontal Seam Shrink.
-	 * Shrinks the image by removing horizontal seams and attempts
-	 * to maximize speed and efficiency rather than showcasing the
-	 * process. (not required by specs)
+	 * Shrinks a given image by removing a given number
+	 * of horizontal seams.
+	 * <p>
+	 * Unlike horizontalSeamShrink, this method is oriented
+	 * towards resizing the image as quickly as possible,
+	 * rather than showcasing the process.
 	 * 
 	 * @param im
-	 * 	the image to operate on.
-	 * @param decHeight
-	 * 	the amount to decrease the height.
+	 * 		the image.
+	 * @param DEC_HEIGHT
+	 * 		the number of horizontal seams to remove.
 	 **/
-	public void fastHSShrink( UWECImage im, int decHeight )
+	public void fastHSShrink( UWECImage im, final int DEC_HEIGHT )
 	{
+		/* Local Variables */
 		UWECImage imT = im.transpose();
 		int[][] energy      = null;
 		int[][] pathWeights = null;
 		int[]   seam        = null;
 		
-		if ( 0 < decHeight )
+		/* Run initial calculations */
+		if ( 0 < DEC_HEIGHT )
 		{
 			energy      = getEnergy( imT );
-			pathWeights = getPathWeights( imT, energy );
-			seam        = getSeam( imT, pathWeights );
+			pathWeights = getPathWeights( energy );
+			seam        = getSeam( pathWeights );
+			
+			/* Remove Seam */
 			imT.switchImage( imT.shrinkImage( seam ) );
 		}
 		
-		for ( int i = 1; i < decHeight; i++ )
+		/* Run optimized calculations */
+		for ( int i = 1; i < DEC_HEIGHT; i++ )
 		{
 			energy      = getEnergy( imT, energy, seam );
-			pathWeights = getPathWeights( imT, energy, pathWeights, seam );
-			seam        = getSeam( imT, pathWeights );
+			pathWeights = getPathWeights( energy, pathWeights, seam );
+			seam        = getSeam( pathWeights );
+			
+			/* Remove Seam */
 			imT.switchImage( imT.shrinkImage( seam ) );
 		}
 		im.switchImage( imT.transpose() );
 		im.repaintCurrentDisplayWindow();
 	}
 	
-	private int[]   getSeam( UWECImage im, int[][] pathWeights );
-	
-	private int[][] getEnergy( UWECImage im );
+	/**
+	 * Creates an energy map for a given image.
+	 * Energy is computed using a dual-gradient energy function.
+	 * 
+	 * @param IM
+	 * 		the given image.
+	 * @return
+	 * 		the energy map.
+	 **/
+	private int[][] getEnergy( final UWECImage IM );
 	
 	/**
-	 * Calculates energy map for an image.
-	 * Attempts to maximize efficiency by not recalculating values
-	 * that will not have changed due to a the last seam removal.
-	 * Instead, it simply copies them from the last energy map.
+	 * Creates an energy map for a given image based on a
+	 * previous energy map and the removed seam.
+	 * Energy is computed using a dual-gradient energy function.
 	 * 
-	 * @param im
-	 * 	the image to operate on.
-	 * @param lastEnergy
-	 * 	the previous energy map.
-	 * @param lastSeam
-	 * 	the last seam removed from the image.
+	 * @param IM
+	 * 		the given image.
+	 * @param LAST_ENERGY
+	 * 		the previous energy map.
+	 * @param LAST_SEAM
+	 * 		the last seam removed.
 	 * @return
-	 * 	the new energy map.
+	 * 		the energy map.
 	 **/
-	private int[][] getEnergy( UWECImage im, int[][] lastEnergy, int[] lastSeam );
-		// only called by fast methods
+	private int[][] getEnergy( final UWECImage IM, final int[][] LAST_ENERGY, final int[] LAST_SEAM );
+		// Only called by fast methods.
+	/**
+	 * Creates a map of the minimum energy required to
+	 * get to every pixel of a given energy map.
+	 * 
+	 * @param ENERGY
+	 * 		the image's energy map.
+	 * @return
+	 * 		the path weight map.
+	 **/
+	private int[][] getPathWeights( final int[][] ENERGY );
 	
 	/**
-	 * Calculates the total energy required to get to each pixel.
-	 * This method basically generates the trace-back table.
+	 * Creates a map of the minimum energy required to
+	 * get to every pixel of a given image based on the
+	 * previous path weight map and the removed seam.
 	 * 
-	 * @param im
-	 * 	the image to operate on.
-	 * @param energy
-	 * 	the energy map of the image.
+	 * @param ENERGY
+	 * 		the image's energy map.
+	 * @param LAST_PATHS
+	 * 		the previous path weight map.
+	 * @param LAST_SEAM
+	 * 		the last seam removed.
 	 * @return
-	 * 	a map of the cost of the minimum path that goes through
-	 * 	each pixel.
+	 * 		the path weight map.
 	 **/
-	private int[][] getPathWeights( UWECImage im, int[][] energy );
-	
-	private int[][] getPathWeights( UWECImage im, int[][] energy, int[][] lastPaths, int[] lastSeam );
-		// only called by fast methods
+	private int[][] getPathWeights( final int[][] ENERGY, final int[][] LAST_PATHS, final int[] LAST_SEAM );
+		// Only called by fast methods.
+	/**
+	 * Calculates the minimum path from the top
+	 * to the bottom of a given image.
+	 * 
+	 * @param PATH_WEIGHTS
+	 * 		the minimum energy required to get to every pixel.
+	 * @return
+	 * 		the minimum path.
+	 **/
+	private int[] getSeam( final int[][] PATH_WEIGHTS );
 }
