3d0077adcd583900b697d41a23c67d706b1e7b80
Aaron Nelson <acnelson12@gmail.com>
2015-10-27 09:24:09 -0500

    Update Seam.java

--- Seam-Carving/Seam.java
+++ Seam-Carving/Seam.java
@@ -191,7 +191,38 @@ public class Seam
      * @return
      *      the energy map.
      **/
-    private int[][] getEnergy( final UWECImage IM );
+    private int[][] getEnergy( final UWECImage IM )
+    {
+        /* Local Constants */
+        final int HEIGHT = IM.getHeight();
+        final int WIDTH  = IM.getWidth();
+        
+        /* Local Variables */
+        int[][] energy = new int[WIDTH][HEIGHT];
+        
+        /* Generate Energy Map */
+        for ( int i = 0; i < WIDTH; i++ )
+            for ( int j = 0; j < HEIGHT; j++ )
+            {
+                /* Block Variables */
+                int deltaRed;
+                int deltaGreen;
+                int deltaBlue;
+                
+                int prevI = ( i + WIDTH - 1 ) % WIDTH;
+                int nextI = ( i + 1 ) % WIDTH;
+                
+                /* Analyze Image */
+                deltaRed   = IM.getRed(   prevI, j ) - IM.getRed(   nextI, j );
+                deltaGreen = IM.getGreen( prevI, j ) - IM.getGreen( nextI, j );
+                deltaBlue  = IM.getBlue(  prevI, j ) - IM.getBlue(  nextI, j );
+                
+                energy[i][j] = deltaRed   * deltaRed +
+                               deltaGreen * deltaGreen +
+                               deltaBlue  * deltaBlue;
+            }
+        return energy;
+    }
     
     /**
      * Creates an energy map for a given image based on a
@@ -207,7 +238,53 @@ public class Seam
      * @return
      *      the energy map.
      **/
-    private int[][] getEnergy( final UWECImage IM, final int[][] LAST_ENERGY, final int[] LAST_SEAM );
+    private int[][] getEnergy( final UWECImage IM,
+                               final int[][]   LAST_ENERGY,
+                               final int[]     LAST_SEAM )
+    {
+        /* Local Constants */
+        final int HEIGHT = IM.getHeight();
+        final int WIDTH  = IM.getWidth();
+        
+        /* Local Variables */
+        int[][] energy = new int[WIDTH][HEIGHT];
+        
+        /* Generate Energy Map */
+        for ( int j = 0; j < HEIGHT; j++ )
+        {
+            /* Copy Values from Old Map */
+            for ( int i = 0; i < LAST_SEAM[0]-j-1; i++ )
+                energy[i][j] = LAST_ENERGY[i][j];
+            
+            for ( int i = LAST_SEAM[0]+j+1; i < WIDTH; i++ )
+                energy[i][j] = LAST_ENERGY[i+1][j];
+            
+            /* Generate New Values */
+            for ( int i = LAST_SEAM[0]-j-1; i <= LAST_SEAM[0]+j &&
+                                            i < WIDTH; i++ )
+            {
+                if ( i < 0 )
+                    i = 0;
+                /* Block Variables */
+                int deltaRed;
+                int deltaGreen;
+                int deltaBlue;
+                
+                int prevI = ( i + WIDTH - 1 ) % WIDTH;
+                int nextI = ( i + 1 ) % WIDTH;
+                
+                /* Analyze Image */
+                deltaRed   = IM.getRed(   prevI, j ) - IM.getRed(   nextI, j );
+                deltaGreen = IM.getGreen( prevI, j ) - IM.getGreen( nextI, j );
+                deltaBlue  = IM.getBlue(  prevI, j ) - IM.getBlue(  nextI, j );
+                
+                energy[i][j] = deltaRed   * deltaRed +
+                               deltaGreen * deltaGreen +
+                               deltaBlue  * deltaBlue;
+            }
+        }
+        return energy;
+    }
     
     /**
      * Creates a map of the minimum energy required to
@@ -218,7 +295,55 @@ public class Seam
      * @return
      *      the path weight map.
      **/
-    private int[][] getPathWeights( final int[][] ENERGY );
+    private int[][] getPathWeights( final int[][] ENERGY )
+    {
+        /* Local Constants */
+        final int HEIGHT = ENERGY[0].length;
+        final int WIDTH  = ENERGY.length;
+        
+        /* Local Variables */
+        int[][] pathWeight = new int[WIDTH][HEIGHT];
+        
+        /* Calculate Minimum Energy Paths */
+        for ( int i = 0; i < WIDTH; i++ )
+            pathWeight[i][0] = ENERGY[i][0];
+        
+        for ( int j = 1; j < HEIGHT; j++ )
+        {
+            /*
+             * Calculate Side Path Weights
+             * (special case)
+             */
+            pathWeight[0][j]       = pathWeight[0][j-1] < pathWeight[1][j-1] ?
+                                     pathWeight[0][j-1] : pathWeight[1][j-1];
+            pathWeight[WIDTH-1][j] = pathWeight[WIDTH-1][j-1] <
+                                     pathWeight[WIDTH-2][j-1] ?
+                                     pathWeight[WIDTH-1][j-1] :
+                                     pathWeight[WIDTH-2][j-1];
+            pathWeight[0][j] += ENERGY[0][j];
+            pathWeight[WIDTH-1][j] += ENERGY[WIDTH-1][j];
+            
+            /* Calculate Middle Path Weights */
+            for ( int i = 1; i < WIDTH-1; i++ )
+            {
+                /* Block Variables */
+                int weightNW = 0;
+                int weightN  = 0;
+                int weightNE = 0;
+                
+                /* Calculate Weights */
+                weightN  = pathWeight[i][j-1];
+                weightNW = pathWeight[i-1][j-1];
+                weightNE = pathWeight[i+1][j-1];
+                
+                pathWeight[i][j] = weightN < weightNW ? weightN : weightNW;
+                pathWeight[i][j] = pathWeight[i][j] > weightNE ?
+                                   weightNE : pathWeight[i][j];
+                pathWeight[i][j] += ENERGY[i][j];
+            }
+        }
+        return pathWeight;
+    }
     
     /**
      * Creates a map of the minimum energy required to
@@ -234,7 +359,58 @@ public class Seam
      * @return
      *      the path weight map.
      **/
-    private int[][] getPathWeights( final int[][] ENERGY, final int[][] LAST_PATHS, final int[] LAST_SEAM );
+    private int[][] getPathWeights( final int[][] ENERGY,
+                                    final int[][] LAST_PATHS,
+                                    final int[]   LAST_SEAM )
+    {
+        /* Local Constants */
+        final int HEIGHT = ENERGY[0].length;
+        final int WIDTH  = ENERGY.length;
+        
+        /* Local Variables */
+        int[][] pathWeights = new int[WIDTH][HEIGHT];
+        int start;
+        int end;
+
+        /* Calculate Minimum Energy Paths */
+        for ( int j = 0; j < HEIGHT; j++ )
+        {
+            start = LAST_SEAM[0]-j;
+            end = LAST_SEAM[0]+j;
+            if ( end > WIDTH )
+                end = WIDTH;
+            if ( start < 0 )
+                start = 0;
+            
+            /* Copy Values from Old Map */
+            for ( int i = 0; i < start; i++ )
+                pathWeights[i][j] = LAST_PATHS[i][j];
+            
+            for ( int i = end; i < WIDTH; i++ )
+                pathWeights[i][j] = LAST_PATHS[i+1][j];
+            
+            /* Calculate New Values */
+            for ( int i = start; i < end; i++ )
+            {
+                /* Block Variables */
+                int weightNW = 0;
+                int weightN  = 0;
+                int weightNE = 0;
+                
+                /* Calculate Weights */
+                weightN  = pathWeights[i][j-1];
+                weightNW = i > 0 ? pathWeights[i-1][j-1] : Integer.MAX_VALUE;
+                weightNE = i+1 < end ? pathWeights[i+1][j-1] :
+                                       Integer.MAX_VALUE;
+                
+                pathWeights[i][j] = weightN < weightNW ? weightN : weightNW;
+                pathWeights[i][j] = pathWeights[i][j] > weightNE ?
+                                   weightNE : pathWeights[i][j];
+                pathWeights[i][j] += ENERGY[i][j];
+            }
+        }
+        return pathWeights;
+    }
     
     /**
      * Calculates the minimum path from the top
@@ -245,5 +421,30 @@ public class Seam
      * @return
      *      the minimum path.
      **/
-    private int[] getSeam( final int[][] PATH_WEIGHTS );
+    private int[] getSeam( final int[][] PATH_WEIGHTS )
+    {
+        /* Local Constants */
+        final int HEIGHT = PATH_WEIGHTS[0].length;
+        final int WIDTH  = PATH_WEIGHTS.length;
+        final int LAST   = HEIGHT-1;
+        
+        /* Local Variables */
+        int[] seam = new int[HEIGHT];
+        
+        /* Find Minimum Path Energy for Bottom Pixel */
+        seam[LAST] = 0;
+        for ( int j = 0; j < WIDTH; j++ )
+            if ( PATH_WEIGHTS[j][LAST] < PATH_WEIGHTS[seam[LAST]][LAST] )
+                seam[LAST] = j;
+        
+        /* Trace Seam */
+        for ( int i = seam.length-2; i >= 0; i-- )
+        {
+            seam[i] = seam[i+1] > 0 ? seam[i+1]-1 : seam[i+1];
+            for ( int j = seam[i+1]; j <= seam[i+1]+1 && j < WIDTH; j++ )
+                if ( PATH_WEIGHTS[j][i] < PATH_WEIGHTS[seam[i]][i] )
+                    seam[i] = j;
+        }
+        return seam;
+    }
 }
